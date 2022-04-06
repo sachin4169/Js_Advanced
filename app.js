a56321989a2b153226a153b9cc7d960a80df9057
sachinyadav <sachinyadav@cedcoss.com>
2022-04-06 19:01:22 +0530

    Create an object of the class and display it

--- app.js
+++ app.js
@@ -7,36 +7,41 @@ var table = `<table>
 </tr>`;
 var tfooter = "</table>";
 var person = [];
-function addproduct() {
-var id = document.getElementById("id").value;
-var name = document.getElementById("Name").value;
-var sal = document.getElementById("sal").value;
+class employe{
 
-var data = {
-Id: `${id}`,
-Name: `${name}`,
-Sal: `${sal}`,
-};
-person.push(data);
-console.log(person);
-display();
+    personfun() {
+        var id = document.getElementById("id").value;
+        var name = document.getElementById("Name").value;
+        var sal = document.getElementById("sal").value;
+
+        var data = {
+        Id: `${id}`,
+        Name: `${name}`,
+        Sal: `${sal}`,
+        };
+        person.push(data);
+        console.log(person);
+        
+    }
+    display(){
+        var row =""
+        person.forEach((element) => {
+        row += `  <tr>
+        <td>${element.Id}</td>
+        <td>${element.Name}</td>
+        <td>${element.Sal}</td>
+        <td class="action">Delete</td>
+        </tr>`;
+        });
+        document.getElementById("table").innerHTML = table +row+ tfooter;
+
+        document.getElementById("id").value = "";
+        document.getElementById("Name").value = "";
+        document.getElementById("sal").value = "";
+    }
 }
-
-function display(){
-var row =""
-person.forEach((element) => {
-row += `  <tr>
-    <td>${element.Id}</td>
-    <td>${element.Name}</td>
-    <td>${element.Sal}</td>
-    <td class="action">Delete</td>
-</tr>`;
-});
-document.getElementById("table").innerHTML = table +row+ tfooter;
-
-document.getElementById("id").value = "";
-document.getElementById("Name").value = "";
-document.getElementById("sal").value = "";
+let e1 = new employe();
+function addperson(){
+e1.personfun();
+e1.display();
 }
-
-
