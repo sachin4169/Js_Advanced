7bc956f8848f13f6600cf774375830cade235fda
sachinyadav <sachinyadav@cedcoss.com>
2022-04-07 11:44:17 +0530

    this keyword to display the employee details

--- app.js
+++ app.js
@@ -9,32 +9,42 @@ var tfooter = "</table>";
 var person = [];
 class employe{
 
-    personfun() {
-        var id = document.getElementById("id").value;
-        var name = document.getElementById("Name").value;
-        var sal = document.getElementById("sal").value;
-
-        var data = {
-        Id: `${id}`,
-        Name: `${name}`,
-        Sal: `${sal}`,
-        };
-        person.push(data); 
-        console.log(person);
-        const myJSON = JSON.stringify(person);
-        console.log(myJSON)
-        
+    constructor(id,name,salary){
+        this.id =id;
+        this.name = name;
+        this.salary = salary;
     }
+
+    // personfun() {
+    //     var id = document.getElementById("id").value;
+    //     var name = document.getElementById("Name").value;
+    //     var sal = document.getElementById("sal").value;
+
+    //     var data = {
+    //     Id: `${id}`,
+    //     Name: `${name}`,
+    //     Sal: `${sal}`,
+    //     };
+    //     person.push(data); 
+    //     console.log(person);
+    //     JSON.stringify(person);
+    //     // console.log(myJSON)
+        
+    // }
     display(){
+        // var data = JSON.parse(person)
+        // console.log(typeof(data))
+        // console.log(data)
+        // console.log(typeof(person));
         var row =""
-        person.forEach((element) => {
+        // person.forEach((element) => {
         row += `  <tr>
-        <td>${element.Id}</td>
-        <td>${element.Name}</td>
-        <td>${element.Sal}</td>
+        <td>${this.id}</td>
+        <td>${this.name}</td>
+        <td>${this.salary}</td>
         <td class="action">Delete</td>
         </tr>`;
-        });
+        // });
         document.getElementById("table").innerHTML = table +row+ tfooter;
 
         document.getElementById("id").value = "";
@@ -42,10 +52,14 @@ class employe{
         document.getElementById("sal").value = "";
     }
 }
-let e1 = new employe();
-console.log(e1)
+
 
 function addperson(){
-e1.personfun();
-e1.display();
+    var id = document.getElementById("id").value;
+    var name = document.getElementById("Name").value;
+    var sal = document.getElementById("sal").value;
+    // e1.personfun();
+    let e1 = new employe(id,name, sal);
+    console.log(e1)
+    e1.display();
 }
